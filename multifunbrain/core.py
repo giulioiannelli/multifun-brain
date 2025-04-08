ab984741d984bbaac86cfbe5217a07971892a02a
giulioiannelli <giulioiannelli.w@gmail.com>
2025-04-08 11:21:08 +0200

    Add bandpass_filter function and import shared module in core.py

--- multifunbrain/core.py
+++ multifunbrain/core.py
@@ -1,2 +1,13 @@
+from .shared import *
+
 def hello_brain(name):
     return f"Hello, {name}! Welcome to multifun-brain."
+
+def bandpass_filter(data, low, high, fs, order=4):
+    nyq = 0.5 * fs
+    b, a = butter(order, [low / nyq, high / nyq], btype='band')
+    return filtfilt(b, a, data)
+
+
+
+
