96dec58c7f1aae4a403597ea93a4bd97dd092296
giulioiannelli <giulioiannelli.w@gmail.com>
2025-04-10 10:36:42 +0200

    Refactor bandpass_filter to accept customizable filter type

--- multifunbrain/core.py
+++ multifunbrain/core.py
@@ -3,11 +3,11 @@ from .shared import *
 def hello_brain(name):
     return f"Hello, {name}! Welcome to multifun-brain."
 
-def bandpass_filter(data, low, high, fs=1, order=4):
+def bandpass_filter(data, low, high, fs=1, order=4, btype='band'):
     nyq = 0.5 * fs
     low_norm = low / nyq
     high_norm = high / nyq
-    b, a = butter(order, [low_norm, high_norm], btype='band')
+    b, a = butter(order, [low_norm, high_norm], btype=btype)
     return filtfilt(b, a, data)
 
 def marchenko_pastur(l, g):
