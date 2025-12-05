a28c5d98c4eff0abd138cd5be0bd362526c4b84c
giulioiannelli <giulioiannelli.w@gmail.com>
2025-12-05 10:56:10 +0100

    Refactor code structure for improved readability and maintainability

--- multifunbrain/notebook/__init__.py
+++ multifunbrain/notebook/__init__.py
@@ -22,17 +22,24 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 import pickle as pk
+import seaborn as sns
 
+
+import plotly.graph_objects as go
 import xml.etree.ElementTree as ET
 
 from contextlib import suppress
 from nilearn import datasets, plotting, image
 from pathlib import Path
+from matplotlib import gridspec
 
 from nilearn.datasets import load_mni152_template
 from scipy.linalg import eigh
+from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score, fowlkes_mallows_score
 from scipy.spatial.distance import squareform
-from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster, \
+    set_link_color_palette
+from sklearn.metrics.cluster import contingency_matrix
 
 from multifunbrain.analysis import *
 from multifunbrain.visualization.plotlib import *
